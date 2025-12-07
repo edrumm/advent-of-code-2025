a77e1c4278aeecbfc4c55729864c59646e765fed
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2025-12-07 01:00:54 +0000

    day 5 pt 2

--- day5/day5_pt2.py
+++ day5/day5_pt2.py
@@ -1,2 +1,16 @@
-# TODO
-# Didn't have time for this one today ;)
+with open('day5.txt', 'r') as f:
+    ranges = [tuple(map(int, l.split('-'))) for l in f.read().split('\n\n')[0].splitlines()]
+
+count = 0
+ranges, merged = sorted(ranges, key=lambda x: x[0]), []
+
+for lo, hi in ranges:
+    if not merged or lo > merged[-1][1]:
+        merged.append(list((lo, hi)))
+    else:
+        merged[-1][1] = max(merged[-1][1], hi)
+
+for r in merged:
+    count += 1 + r[1] - r[0]
+
+print(count)
