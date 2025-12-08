6c1d9acedbdd1971a29aaf734268b11160232c2d
edrumm <esd0658@googlemail.com>
2025-12-08 21:15:38 +0000

    day 8

---
+++ day8/day8_pt1.py
@@ -0,0 +1,24 @@
+from scipy.cluster.hierarchy import DisjointSet
+from itertools import combinations
+from math import sqrt, prod
+
+
+# Helper func - return the 3D euclidean distance between 2 points
+def dist3d(p1, p2):
+    x1, y1, z1 = p1; x2, y2, z2 = p2
+    return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+
+
+with open('day8.txt', 'r') as f:
+   boxes = [tuple(map(int, ln.split(','))) for ln in f.readlines()]
+
+distances = sorted(combinations(boxes, 2), key=lambda x: dist3d(*x))
+ds = DisjointSet(boxes)
+
+for i, d in enumerate(distances):
+    if i == 1000:
+        break
+    ds.merge(*d)
+
+print(prod(sorted(map(len, ds.subsets()))[-3:]))
+
